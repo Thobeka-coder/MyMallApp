50b62e4122d9390b392f21aca52702ee8e372d67
Thobeka-coder <thobekasithole3@gmail.com>
2021-09-25 14:49:00 +0200

    mymall app

---
+++ MyMall/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+//import { ServicesComponent } from './services/services.component';
+//import { AboutUsComponent } from './about-us/about-us.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+//import { UserComponent } from './user/user.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { PageFooterComponent } from './page-footer/page-footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    HomeComponent,
+    //ServicesComponent,
+    //AboutUsComponent,
+    LoginComponent,
+    PageFooterComponent,
+    //UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
